9b365bb6e86fe0d98c311d36618eb0bea569e1a9
lucaasleao <lucashenriquemarinholeao@gmail.com>
2022-03-15 19:06:27 -0300

    first commit

---
+++ tarefacasting/tarefa09/src/tarefa9/conversaodosvalores.java
@@ -0,0 +1,12 @@
+package tarefa9;
+
+public class conversaodosvalores {
+    public static void main (String args[]){
+        int nota1 = 10;
+        System.out.println(nota1);
+        short nota2 = (short) nota1;
+        System.out.println(nota2);
+
+
+    }
+}
