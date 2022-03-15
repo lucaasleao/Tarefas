7a765c25b6e73640f516466c475e49fcf9e622e4
lucaasleao <lucashenriquemarinholeao@gmail.com>
2022-03-14 23:08:55 -0300

    first commit

---
+++ tarefacalculomedia/src/CalculoDaMedia.java
@@ -0,0 +1,21 @@
+public class CalculoDaMedia {
+    public static void main(String args[]) {
+        calculoMedia();
+
+        }
+
+    public static void calculoMedia (){
+        System.out.println("** Calculo da média **");
+        int nota1 = 10;
+        int nota2 = 20;
+        int nota3 = 25;
+        int nota4 = 5;
+        int somadasnotas = nota1 + nota2 + nota3+ nota4;
+        int media = somadasnotas/4;
+        System.out.println(media);
+
+
+
+    }
+
+}
